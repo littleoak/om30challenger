345df9f10beca032f2f2dcd05f81c24226460dbc
littleoak <whilelive@gmail.com>
2023-03-31 10:03:08 -0300

    roadmap editado + migrations/models criadas de Pacientes/Endereços

--- app/Http/Controllers/Api/PacientesController.php
+++ app/Http/Controllers/Api/PacientesController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class PacientesController extends Controller
 {
-    //
+    #mostrar pacientes overall, quando está listando sem critérios - bom paginar já
+    public function index()
+    {
+
+    }
+
+    #mostrar paciente por search, paginar aqui também é bom
+    public function show()
+    {
+
+    }
+
+    #insert do paciente
+    public function store()
+    {
+
+    }
+
+    #atualizando um dado do paciente
+    public function update()
+    {
+
+    }
+
+    #removendo um paciente
+    public function destroy()
+    {
+
+    }
+    
 }
