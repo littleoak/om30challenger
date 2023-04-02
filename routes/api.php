8670453d11046018b0bdd48cc220619ae8912670
littleoak <whilelive@gmail.com>
2023-04-02 01:59:47 -0300

    Add breeze + api route + relacionamentos nas models

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/pacientes', \App\Http\Controllers\Api\PacientesController::class);
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource(
+        '/pacientes',
+        \App\Http\Controllers\Api\PacientesController::class);
+});
 
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    if(Auth::attempt($credentials) === false) {
+        return response()->json('Unauthorized', 401);
+    }
+
+    $user = Auth::user();
+    $user->tokens()->delete(); //revogar um token anterior
+    //autorizações, padrão é ['*'] <--- pode tudo
+    //$token = $user->createToken('token', ['pacientes:delete']); //se a gente for pensar em limitar o escopo
+    $token = $user->createToken('token', ['*']); //escopo máximo
+    return response()->json($token->plainTextToken);
+});
