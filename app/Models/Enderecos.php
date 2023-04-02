1cbce4a4b41bee0c3b94905c7c34e5eed4de045d
littleoak <whilelive@gmail.com>
2023-04-02 01:26:28 -0300

    trabalhando nas models agoram, adicionando Attribute para tentar gerar links e criando relacionamento de 1 para 1 do Pacientes -> Endereços com hasOne e em enderecos o belongsTo

--- app/Models/Enderecos.php
+++ app/Models/Enderecos.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Enderecos extends Model
 {
     use HasFactory;
+
+    public function pacientes(): BelongsTo
+    {
+        return $this->belongsTo(Pacientes::class);
+    }
 }
--- app/Models/Pacientes.php
+++ app/Models/Pacientes.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pacientes extends Model
 {
     use HasFactory;
+    protected $appends = ['link'];
+    protected $with = ['enderecos'];
+
+    public function enderecos()
+    {
+        return $this->hasOne(Enderecos::class);
+    }
+
+    public function link(): Attribute //com appends a gente disse que LINK que não é uma coluna na tabela
+        //deve ser chamado, no topo na var appends
+    {
+        return new Attribute(
+            get: fn() => [[
+                'rel' => 'self',
+                'url' => "/api/pacientes/{$this->id}"
+            ],
+                [
+                    'rel' => 'enderecos',
+                    'url' => "/api/pacientes/{$this->id}/enderecos"
+                ],
+            ]);
+    }
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered', function(Builder $queryBuilder) {
+            $queryBuilder->orderBy('nome', 'asc');
+        });
+    }
+
+
 }
